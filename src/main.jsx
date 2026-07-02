5d41a67ca94884c257af667e268fd61b7910550e
Tanay Naik <tanaynaik0710@gmail.com>
2026-07-02 21:29:59 +0530

    deployment changes 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HashRouter } from 'react-router-dom';
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/tanay-portfolio-v2">
-      <App />
-    </BrowserRouter>
+    <HashRouter>
+  <App />
+</HashRouter>
   </StrictMode>,
 )
